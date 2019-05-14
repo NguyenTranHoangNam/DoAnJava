4fd77085ad5f2884f7a1250be596db4386500698
nguyenbaovan <baovan2208@gmail.com>
2019-05-14 12:40:22 +0700

    Update dieu kien + Form Bao cao thang

--- QuanLySTK/src/SwingUI/BaoCaoTHang.java
+++ QuanLySTK/src/SwingUI/BaoCaoTHang.java
@@ -6,26 +6,22 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.JScrollBar;
 import javax.swing.JLabel;
-import javax.swing.SwingConstants;
-import javax.swing.JTable;
-import javax.swing.JScrollPane;
+import java.awt.Color;
+import java.awt.Font;
 import javax.swing.JTextField;
-import javax.swing.JComboBox;
-import com.toedter.calendar.JDayChooser;
-import com.toedter.calendar.JDateChooser;
-import com.toedter.calendar.JMonthChooser;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class BaoCaoTHang extends JFrame {
+public class BaoCaoThang extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtLoaiTietKiem;
+	private JTextField txtThang;
 	private JTable table;
-	private DefaultTableModel defaultTable_1;
 
 	/**
 	 * Launch the application.
@@ -34,7 +30,7 @@ public class BaoCaoTHang extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					BaoCaoTHang frame = new BaoCaoTHang();
+					BaoCaoThang frame = new BaoCaoThang();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -46,51 +42,64 @@ public class BaoCaoTHang extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public BaoCaoTHang() {
+	public BaoCaoThang() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 597, 326);
+		setBounds(100, 100, 478, 336);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("BAO CAO MO / DONG SO THANG");
-		lblNewLabel.setBounds(5, 5, 440, 16);
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		contentPane.add(lblNewLabel);
+		JLabel lblBaoCao = new JLabel("B\u00C1O C\u00C1O M\u1EDE/\u0110\u00D3NG S\u1ED4 TH\u00C1NG");
+		lblBaoCao.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblBaoCao.setForeground(Color.BLUE);
+		lblBaoCao.setBounds(126, 11, 239, 23);
+		contentPane.add(lblBaoCao);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(121, 50, 111, 27);
-		contentPane.add(comboBox);
+		JLabel lblLoaiTietkiem = new JLabel("Lo\u1EA1i ti\u1EBFt ki\u1EC7m:");
+		lblLoaiTietkiem.setBounds(35, 54, 73, 14);
+		contentPane.add(lblLoaiTietkiem);
 		
-		JLabel lblLoaiTietKiem = new JLabel("LOAI TIET KIEM");
-		lblLoaiTietKiem.setBounds(14, 53, 95, 16);
-		contentPane.add(lblLoaiTietKiem);
-		
-		JLabel lblThang = new JLabel("THANG");
-		lblThang.setBounds(244, 54, 61, 16);
+		JLabel lblThang = new JLabel("Th\u00E1ng:");
+		lblThang.setBounds(250, 54, 73, 14);
 		contentPane.add(lblThang);
 		
-		JMonthChooser monthChooser = new JMonthChooser();
-		monthChooser.setBounds(296, 50, 137, 26);
-		contentPane.add(monthChooser);
+		txtLoaiTietKiem = new JTextField();
+		txtLoaiTietKiem.setBounds(110, 51, 118, 20);
+		contentPane.add(txtLoaiTietKiem);
+		txtLoaiTietKiem.setColumns(10);
 		
-		JButton btnTimKiem = new JButton("TIM KIEM");
-		btnTimKiem.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		txtThang = new JTextField();
+		txtThang.setColumns(10);
+		txtThang.setBounds(303, 51, 118, 20);
+		contentPane.add(txtThang);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+			},
+			new String[] {
+				"STT", "Ng\u00E0y", "S\u1ED1 M\u1EDF", "S\u1ED1 \u0110\u00F3ng", "Ch\u00EAnh L\u1EC7ch"
+			}
+		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(57);
+		table.getColumnModel().getColumn(1).setPreferredWidth(127);
+		table.getColumnModel().getColumn(2).setPreferredWidth(97);
+		table.getColumnModel().getColumn(3).setPreferredWidth(84);
+		table.getColumnModel().getColumn(4).setPreferredWidth(94);
+		table.setBounds(10, 97, 442, 132);
+		contentPane.add(table);
+		
+		JButton btnOK = new JButton("OK");
+		btnOK.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
 			}
 		});
-		btnTimKiem.setBounds(457, 49, 117, 29);
-		contentPane.add(btnTimKiem);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(14, 98, 560, 181);
-		contentPane.add(scrollPane);
-		
-		defaultTable_1 = new DefaultTableModel(new Object[][] {},new String[] {
-				"STT","MA SO","LOAI TIET KIEM","KHACH HANG","SO DU"	
-			});
-			table = new JTable(defaultTable_1);
-			scrollPane.setViewportView(table);
+		btnOK.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnOK.setBounds(363, 250, 89, 23);
+		contentPane.add(btnOK);
 	}
 }
